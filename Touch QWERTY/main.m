b4d09d845930a3756a7b2785dfdf5a9d9fea82fc
tph <antek.piechnik@gmail.com>
2009-11-05 10:26:56 +0100

    Initial XCode project

---
+++ Touch QWERTY/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Touch QWERTY
+//
+//  Created by Antek Piechnik on 11/5/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
